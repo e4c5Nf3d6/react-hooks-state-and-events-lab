3dcdadee474d43adda5e83c1f3fd10a465d735c4
Maria Jenkins <macjenkin@gmail.com>
2023-06-11 11:15:22 -0500

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, editCart] = useState('add')
+
+  function handleCart(e) {
+    if (e.target.className === 'add') {
+      editCart('remove')
+    } else {
+      editCart('add')
+    }
+  }
+
+  const liClass = inCart === 'add' ? "" : "in-cart"
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart} onClick={handleCart}>{inCart === 'add' ? 'Add to Cart' : 'Remove From Cart'}</button>
     </li>
   );
 }
